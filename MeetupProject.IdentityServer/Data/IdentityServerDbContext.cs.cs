04bcf38e155a58f005316ec1bf213130e20d2c87
Aliaksandr Bazylchyk <bazylchyk.a@gmail.com>
2022-07-26 08:51:34 +0300

    Identity Data moved to another database

---
+++ MeetupProject.IdentityServer/Data/IdentityServerDbContext.cs.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetupProject.IdentityServer.Data
+{
+    public class IdentityServerDbContext : IdentityDbContext<IdentityUser<Guid>, IdentityRole<Guid>, Guid>
+    {
+        public IdentityServerDbContext(DbContextOptions<IdentityServerDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
